cd35be1b1a734a023f615c13d6f4ab1e9835fdfb
Suraj Kumar <surajbansikumar@gmail.com>
2023-11-07 21:06:38 +0530

    setting changed

--- CIBBL/settings.py
+++ CIBBL/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3ze53r%+w*y9ayiw=#+2ss99h@ncpib3!%47$1x8c+$vrj=cy$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
